abd68381c2ea367ef787a2a75c45e28a4061e594
Luis Cerutti <luissantimcerutti@hotmail.com>
2022-07-05 22:58:09 -0300

    commit 1

---
+++ listaDeAlunos.js
@@ -0,0 +1,13 @@
+// let numeroDeAlunos = 10
+let listaDeAlunos = ["Flavio","Gutavo","Nicolas","Mauricio"]
+// array  -> coleção
+
+for (let nome of listaDeAlunos) {
+    console.log("O nome do aluno é " + nome + "no EAD Senai")
+    //concatenador +
+}
+
+for (let nome of listaDeAlunos) {
+    console.log(`O nome do aluno é ${nome} no EAD Senai`)
+    //Interpolador +
+}
